43e10dc6138552d460d5f0adef374bc3c61e3fab
Marc Velez <marcvelez13@gmail.com>
2022-06-08 22:38:28 -0400

    commit tree 2a1e95601a94688e9c76be1cb3d5812984f57c24

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31 ;
+let num2 = 2 ;
+let multiply = num1 * num2 ;
+
+let random = 1
+random = math.ceil(1, random)
+
+let num3 = 29 ;
+let num4 = 5 ;
+let mod = num3 % num4 ;
+
+
+let max = 20 ;
+max = math.max(1 , 4, 6, 20);
 
 // Write your code here
